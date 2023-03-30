fd7a38b25fcf7ed1c32e58393d284a4043e41337
Eskil Heyn Olsen <eskil@eskil.org>
2023-03-29 19:39:50 -0700

    Allow elixir 1.13

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Scurry.MixProject do
       aliases: aliases(),
       app: :scurry,
       version: "2.0.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
